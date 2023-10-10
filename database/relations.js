7663d25b671b9fa41297578a2c50a3a233db92cd
RobertoNeiraGonzalez <roberneira31@gmail.com>
2023-10-10 12:31:28 +0100

    added index.route & relations function

---
+++ database/relations.js
@@ -0,0 +1,15 @@
+const User = require('../api/models/user.model')
+const Comment = require('../api/models/comment.model')
+const Device = require('../api/models/device.model')
+const Medical = require('../api/models/medical_info.model')
+const Objetive = require('../api/models/objetive.model')
+
+function addRelationsToModels() {
+  try {
+    
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {addRelationsToModels}
