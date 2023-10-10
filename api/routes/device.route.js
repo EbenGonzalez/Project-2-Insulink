f42ec877d9c80fce571738657c4965e2ec0a2126
eben <ebencito88@gmail.com>
2023-10-10 19:36:13 +0100

    auth middlewares

--- api/routes/device.route.js
+++ api/routes/device.route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { getAllDevices,getOneDevice,createDevice,updateDevice,deleteDevice } =require ("../controllers/device.controller")
+const{ checkAuth,checkAdmin,checkDoctor }=require("../middlewares/index")
 
 router.get('/all', getAllDevices)
 router.get('/:id', getOneDevice)
