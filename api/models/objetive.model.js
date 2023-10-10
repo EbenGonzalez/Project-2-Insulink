95c97e96cec07936443669aaeff51b371f77a4e9
eben <ebencito88@gmail.com>
2023-10-10 12:56:56 +0100

    minor fixes

--- api/models/objetive.model.js
+++ api/models/objetive.model.js
@@ -3,7 +3,7 @@ const { connection } = require('../../database/index')
 
 const Objetive = connection.define('objetive',
     {
-      serial_number: {
+      Objetive: {
         type: DataTypes.ENUM("good","medium","bad"),
       },
     },
