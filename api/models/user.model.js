ce1827fa36c440a99e4186393ce74c86d1e0a2ed
RobertoNeiraGonzalez <roberneira31@gmail.com>
2023-10-10 12:08:19 +0100

    added user.model

---
+++ api/models/user.model.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("../../database");
+
+const User = connection.define("user", {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+      notNull: {
+        msg: "Please enter your mail",
+      },
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [4, 12],
+    },
+  },
+  phone: {
+    type: DataTypes.INTEGER,
+    unique: true,
+    allowNull: false,
+  },
+  birth_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  debut_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM,
+    values: ['user', 'doctor', 'admin'],
+    defaultValue: 'user'
+  }
+},
+{
+  timestamps: false
+}
+);
