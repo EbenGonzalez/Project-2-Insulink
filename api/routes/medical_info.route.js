92c041706bfa0cf6a9d65001166a3b672a0e2d2f
RobertoNeiraGonzalez <roberneira31@gmail.com>
2023-10-11 21:09:05 +0100

    medical controllers routes done

---
+++ api/routes/medical_info.route.js
@@ -0,0 +1,34 @@
+const router = require('express').Router()
+
+const { 
+  getAllMedical,
+  getOneMedical, 
+  createMedical, 
+  updateMedical, 
+  deleteMedical, 
+  getOwnMedical, 
+  updateOwnMedical, 
+  deleteOwnMedical, 
+ createOwnMedical
+} = require("../controllers/medical_info.controller")
+
+  const { 
+    checkAuth, 
+    checkAdmin, 
+    checkDoctor } = require("../middlewares/index")
+
+   const { 
+    signup, 
+    login } = require("../controllers/auth.controller")
+
+router.get('/me', checkAuth, getOwnMedical)//
+router.get('/', checkAuth, checkDoctor, getAllMedical)//
+router.get('/:id', checkAuth, checkDoctor, getOneMedical)//
+router.post('/me', checkAuth, createOwnMedical)//
+router.post('/:id', checkAuth, checkDoctor, createMedical)//
+router.put('/me',checkAuth, updateOwnMedical)//
+router.put('/:id', checkAuth, checkDoctor, updateMedical)//
+router.delete('/me', checkAuth, deleteOwnMedical)//
+router.delete('/:id', checkAuth, checkDoctor, deleteMedical)//
+
+module.exports = router
