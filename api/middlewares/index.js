21f1e781758553a29109240f26db55a9392f441e
RobertoNeiraGonzalez <roberneira31@gmail.com>
2023-10-16 13:22:06 +0100

    added changes by main branch

--- api/middlewares/index.js
+++ api/middlewares/index.js
@@ -1,35 +1,36 @@
-const jwt=require("jsonwebtoken")
-const User=require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const User = require("../models/user.model")
 
-const checkAuth=(req,res,next)=>{
-    if(!req.headers.authorization){
+const checkAuth = (req, res, next) => {
+    if (!req.headers.authorization) {
         return res.status(401).send("Need a token to continue...")
     }
-    jwt.verify(req.headers.authorization,process.env.SECRET,async(err,result)=>{
-        if(err) return res.send("Invalid Token")
-        const user=await User.findOne({
-    where:{
-       email:result.email
-    }})
-    res.locals.user=user
-    next()
+    jwt.verify(req.headers.authorization, process.env.SECRET, async (err, result) => {
+        if (err) return res.send("Invalid Token")
+        const user = await User.findOne({
+            where: {
+                email: result.email
+            }
+        })
+        res.locals.user = user
+        next()
     })
 }
 
-const checkAdmin=(req,res,next)=>{
-    if(res.locals.user.role !=="admin"){
+const checkAdmin = (req, res, next) => {
+    if (res.locals.user.role !== "admin") {
         return res.status(401).send("You are not an Admin")
-    }else{
+    } else {
         next()
     }
 }
 
-const checkDoctor=(req,res,next)=>{
-    if(res.locals.user.role !=="doctor"){
+const checkDoctor = (req, res, next) => {
+    if (res.locals.user.role !== "doctor") {
         return res.status(401).send("You are not a Doctor")
-    }else{
+    } else {
         next()
     }
 }
 
-module.exports={checkAuth,checkAdmin,checkDoctor}
+module.exports = { checkAuth, checkAdmin, checkDoctor }
