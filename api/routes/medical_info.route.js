9c95f826894e1fc31860c5d34ad370d4c9d9bac7
RobertoNeiraGonzalez <roberneira31@gmail.com>
2023-10-16 11:28:37 +0100

    modified routes medical info

--- api/routes/medical_info.route.js
+++ api/routes/medical_info.route.js
@@ -1,32 +1,28 @@
 const router = require('express').Router()
 
-const { 
+const {
   getAllMedical,
-  getOneMedical, 
-  createMedical, 
-  updateMedical, 
-  deleteMedical, 
-  getOwnMedical, 
-  updateOwnMedical, 
-  deleteOwnMedical, 
- createOwnMedical
+  getOneMedical,
+  createMedical,
+  updateMedical,
+  deleteMedical,
+  getOwnMedical,
+  updateOwnMedical,
+  deleteOwnMedical,
+  createOwnMedical
 } = require("../controllers/medical_info.controller")
 
-  const { 
-    checkAuth, 
-    checkAdmin, 
-    checkDoctor } = require("../middlewares/index")
-
-   const { 
-    signup, 
-    login } = require("../controllers/auth.controller")
+const {
+  checkAuth,
+  checkAdmin,
+  checkDoctor } = require("../middlewares/index")
 
 router.get('/me', checkAuth, getOwnMedical)//
 router.get('/', checkAuth, checkDoctor, getAllMedical)//
 router.get('/:id', checkAuth, checkDoctor, getOneMedical)//
 router.post('/me', checkAuth, createOwnMedical)//
 router.post('/:id', checkAuth, checkDoctor, createMedical)//
-router.put('/me',checkAuth, updateOwnMedical)//
+router.put('/me', checkAuth, updateOwnMedical)//
 router.put('/:id', checkAuth, checkDoctor, updateMedical)//
 router.delete('/me', checkAuth, deleteOwnMedical)//
 router.delete('/:id', checkAuth, checkDoctor, deleteMedical)//
