e02fc324eda1533e6a2b1aae2becf948b91c47f4
eben <ebencito88@gmail.com>
2023-10-17 15:28:37 +0100

    Utilities minor fixes

--- api/controllers/utilities.controller.js
+++ api/controllers/utilities.controller.js
@@ -13,6 +13,7 @@ async function getOwnInsulin(req,res){
         for(let i=0;i<medical.length;i++){
             insulin+=medical[i].basal_insulin+medical[i].bolus_insulin
         }
+      insulin=insulin/medical.length
       return res.status(200).json({ message: 'This Is Your Total Insulin', insulin})
     } else {
       return res.status(404).send('You have not Medical Info Defined')
@@ -34,6 +35,7 @@ async function getInsulin(req,res){
         for(let i=0;i<medical.length;i++){
             insulin+=medical[i].basal_insulin+medical[i].bolus_insulin
         }
+        insulin=insulin/medical.length
       return res.status(200).json({ message: `This Is Patient ${req.params.id} Total Insulin`, insulin})
     } else {
       return res.status(404).send('You have not Medical Info Defined')
@@ -58,6 +60,7 @@ async function getOwnRatio(req,res){
             totalBolus+=medical[i].bolus_insulin
         }
         let ratio=totalBolus/(totalCh/10)
+        ratio=ratio/medical.length
       return res.status(200).json(`You need ${ratio.toFixed(2)} insulin units for each 10 carbohydrates`)
     } else {
       return res.status(404).send('You have not Medical Info Defined')
@@ -82,6 +85,7 @@ async function getRatio(req,res){
             totalBolus+=medical[i].bolus_insulin
         }
         let ratio=totalBolus/(totalCh/10)
+        ratio=ratio/medical.length
       return res.status(200).json(`Patient needs ${ratio.toFixed(2)} insulin units for each 10 carbohydrates`)
     } else {
       return res.status(404).send('You have not Medical Info Defined')
@@ -147,6 +151,7 @@ async function getOwnCh(req,res){
         for(let i=0;i<medical.length;i++){
             totalCh+=medical[i].breakfast_CH+medical[i].lunch_CH+medical[i].snack_CH+medical[i].dinner_CH+medical[i].extra_CH
         }
+        totalCh=totalCh/medical.length
       return res.status(200).json(`Your total carbohydrate consumption has been: ${totalCh}gr.`)
     } else {
       return res.status(404).send('You have not Medical Info Defined')
@@ -168,6 +173,7 @@ async function getCh(req,res){
         for(let i=0;i<medical.length;i++){
             totalCh+=medical[i].breakfast_CH+medical[i].lunch_CH+medical[i].snack_CH+medical[i].dinner_CH+medical[i].extra_CH
         }
+        totalCh=totalCh/medical.length
       return res.status(200).json(`Patient total carbohydrate consumption has been: ${totalCh}gr.`)
     } else {
       return res.status(404).send('You have not Medical Info Defined')
